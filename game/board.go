881a25c1b4d8f10236688d084b82bce3591f2d02
Alex  Morte <alexandersonmbm@gmail.com>
2023-04-18 10:11:58 -0300

    feat: improve typing system

--- game/board.go
+++ game/board.go
@@ -1,19 +1,28 @@
 package game
 
+type FlagPlayer int
+
+const (
+	Player1 FlagPlayer = -1
+	Player2 FlagPlayer = 1
+	NoWin FlagPlayer = 0
+)
+
 type Board struct {
-	points [3][3]int
-	FlagPlayer1 int
-	FlagPlayer2 int
+	points [3][3]FlagPlayer
+	player1 FlagPlayer
+	player2 FlagPlayer
 } 
 
-func (b Board) Init() Board {
-	b.FlagPlayer1 = -1
-	b.FlagPlayer2 = 1
-	return b
+func NewBoard() *Board {
+	return &Board{
+		player1: Player1,
+		player2: Player2,
+	}
 }
 
-func (b *Board) CheckWinner() int {
-	winner := 0
+func (b *Board) CheckWinner() FlagPlayer {
+	winner := NoWin
 	for i := 0; i < 3 && winner==0; i++ {
 		for j := 0; j < 3 && winner==0; j++ {
 			switch {
@@ -29,13 +38,13 @@ func (b *Board) CheckWinner() int {
 	return winner
 }
 
-func (board *Board) checkColumn(column int) int {
+func (board *Board) checkColumn(column int) FlagPlayer {
 	win_one, win_two :=  true, true
 	for i := 0; i < 3; i++ {
 		colValue := board.points[i][column]
-		if colValue == board.FlagPlayer1 { 
+		if colValue == Player1 { 
 			win_two = false
-		} else if colValue == board.FlagPlayer2 { 
+		} else if colValue == Player2 { 
 			win_one = false 
 		} else { 
 			win_one, win_two = false, false
@@ -43,21 +52,21 @@ func (board *Board) checkColumn(column int) int {
 	}
 
 	if win_two { 
-		return board.FlagPlayer2
+		return Player2
 	} else if win_one {
-		return board.FlagPlayer1
+		return Player1
 	}
-	return 0
+	return NoWin
 }
 
 
-func (board *Board) checkRow(row int) int {
+func (board *Board) checkRow(row int) FlagPlayer {
 	win_one, win_two :=  true, true
 	for i := 0; i < 3; i++ {
 		colValue := board.points[row][i]
-		if colValue == board.FlagPlayer1 { 
+		if colValue == Player1 { 
 			win_two = false
-		} else if colValue == board.FlagPlayer2 { 
+		} else if colValue == Player2 { 
 			win_one = false 
 		} else { 
 			win_one, win_two = false, false
@@ -65,21 +74,21 @@ func (board *Board) checkRow(row int) int {
 	}
 
 	if win_two { 
-		return board.FlagPlayer2
+		return Player2
 	} else if win_one {
-		return board.FlagPlayer1
+		return Player1
 	}
-	return 0
+	return NoWin
 }
 
 
-func (board *Board) checkDiagonal(row int) int {
+func (board *Board) checkDiagonal(row int) FlagPlayer {
 	win_one, win_two :=  true, true
 	for i := 0; i < 3; i++ {
 		colValue := board.points[i][i]
-		if colValue == board.FlagPlayer1 { 
+		if colValue == Player1 { 
 			win_two = false
-		} else if colValue == board.FlagPlayer2 { 
+		} else if colValue == Player2 { 
 			win_one = false 
 		} else { 
 			win_one, win_two = false, false
@@ -87,31 +96,31 @@ func (board *Board) checkDiagonal(row int) int {
 	}
 
 	if win_two { 
-		return board.FlagPlayer2
+		return Player2
 	} else if win_one {
-		return board.FlagPlayer1
+		return Player1
 	}
 
 
 	win_one, win_two =  true, true
 	for i := 0; i < 3; i++ {
 		colValue := board.points[i][2-i]
-		if colValue == board.FlagPlayer1 { 
+		if colValue == Player1 { 
 			win_two = false
-		} else if colValue == board.FlagPlayer2 { 
+		} else if colValue == Player2 { 
 			win_one = false 
 		} else { 
 			win_one, win_two = false, false
 		}
 	}
 
-	return 0
+	return NoWin
 }
 
-func (b *Board) Mark(player int, i int, j int) {
+func (b *Board) Mark(player FlagPlayer, i int, j int) {
 	b.points[i][j] = player
 }
 
-func (b *Board) GetMarkAt(i int, j int) int {
+func (b *Board) GetMarkAt(i int, j int) FlagPlayer {
 	return b.points[i][j]
 }
--- game/board_test.go
+++ game/board_test.go
@@ -1,48 +1,47 @@
-package tests
+package game
 
 import (
 	"testing"
-	"tictactoe/game"
 )
 
 func TestCheckMark(test *testing.T) {
-	var board game.Board
-	board.Mark(board.FlagPlayer1, 0, 0)
+	board := NewBoard()
+	board.Mark(Player1, 0, 0)
 	got := board.GetMarkAt(0, 0)
-	expected := board.FlagPlayer1
+	expected := Player1
 	if expected != got {
 		test.Errorf("Expected: %v Received: %v", expected, got)
 	}
 }
 
 func TestCheckWin(test *testing.T) {
-	board := new(game.Board).Init()
-	board.Mark(board.FlagPlayer1, 0, 0)
-	board.Mark(board.FlagPlayer1, 1, 1)
-	board.Mark(board.FlagPlayer1, 2, 2)
+	board := NewBoard()
+	board.Mark(Player1, 0, 0)
+	board.Mark(Player1, 1, 1)
+	board.Mark(Player1, 2, 2)
 	winner := board.CheckWinner()
-	if winner != board.FlagPlayer1 {
-		test.Errorf("Expected: %v Received: %v", board.FlagPlayer1, winner)
+	if winner != Player1 {
+		test.Errorf("Expected: %v Received: %v", Player1, winner)
 	}
 
-	board = new(game.Board).Init()
-	board.Mark(board.FlagPlayer2, 0, 0)
-	board.Mark(board.FlagPlayer2, 1, 1)
-	board.Mark(board.FlagPlayer2, 2, 2)
+	board = NewBoard()
+	board.Mark(Player2, 0, 0)
+	board.Mark(Player2, 1, 1)
+	board.Mark(Player2, 2, 2)
 	winner = board.CheckWinner()
-	if winner != board.FlagPlayer2 {
-		test.Errorf("Expected: %v Received: %v", board.FlagPlayer2, winner)
+	if winner != Player2 {
+		test.Errorf("Expected: %v Received: %v", Player2, winner)
 	}
 
-	board = new(game.Board).Init()
-	board.Mark(board.FlagPlayer2, 0, 1)
-	board.Mark(board.FlagPlayer2, 1, 1)
-	board.Mark(board.FlagPlayer2, 2, 1)
-	board.Mark(board.FlagPlayer1, 0, 0)
-	board.Mark(board.FlagPlayer1, 1, 0)
+	board = NewBoard()
+	board.Mark(Player2, 0, 1)
+	board.Mark(Player2, 1, 1)
+	board.Mark(Player2, 2, 1)
+	board.Mark(Player1, 0, 0)
+	board.Mark(Player1, 1, 0)
 
 	winner = board.CheckWinner()
-	if winner != board.FlagPlayer2 {
-		test.Errorf("Expected: %v Received: %v", board.FlagPlayer2, winner)
+	if winner != Player2 {
+		test.Errorf("Expected: %v Received: %v", Player2, winner)
 	}
 }
